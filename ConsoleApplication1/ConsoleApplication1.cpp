9db051bf20a2f594c80791370cf6c7e38b96bc29
olegzig <boris37544@gmail.com>
2024-05-05 13:40:16 +0300

    Внезапное возникновение кода

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,121 @@
-﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
-
-int main()
+// структура для представления узлов дерева
+struct node 
 {
-    std::cout << "Hello World!\n";
+	int key;
+	unsigned char height;
+	node* left;
+	node* right;
+	node(int k) { key = k; left = right = 0; height = 1; }
+};
+
+unsigned char height(node* p)
+{
+	return p ? p->height : 0;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+int bfactor(node* p)
+{
+	return height(p->right) - height(p->left);
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+void fixheight(node* p)
+{
+	unsigned char hl = height(p->left);
+	unsigned char hr = height(p->right);
+	p->height = (hl > hr ? hl : hr) + 1;
+}
+
+// правый поворот вокруг p
+node* rotateright(node* p) 
+{
+	node* q = p->left;
+	p->left = q->right;
+	q->right = p;
+	fixheight(p);
+	fixheight(q);
+	return q;
+}
+
+// левый поворот вокруг q
+node* rotateleft(node* q)
+{
+	node* p = q->right;
+	q->right = p->left;
+	p->left = q;
+	fixheight(q);
+	fixheight(p);
+	return p;
+}
+
+// балансировка узла p
+node* balance(node* p) 
+{
+	fixheight(p);
+	if (bfactor(p) == 2)
+	{
+		if (bfactor(p->right) < 0)
+			p->right = rotateright(p->right);
+		return rotateleft(p);
+	}
+	if (bfactor(p) == -2)
+	{
+		if (bfactor(p->left) > 0)
+			p->left = rotateleft(p->left);
+		return rotateright(p);
+	}
+	return p; // балансировка не нужна
+}
+
+// вставка ключа k в дерево с корнем p
+node* insert(node* p, int k) 
+{
+	if (!p) return new node(k);
+	if (k < p->key)
+		p->left = insert(p->left, k);
+	else
+		p->right = insert(p->right, k);
+	return balance(p);
+}
+
+// поиск узла с минимальным ключом в дереве p 
+node* findmin(node* p) 
+{
+	return p->left ? findmin(p->left) : p;
+}
+
+// удаление узла с минимальным ключом из дерева p
+node* removemin(node* p) 
+{
+	if (p->left == 0)
+		return p->right;
+	p->left = removemin(p->left);
+	return balance(p);
+}
+
+// удаление ключа k из дерева p
+node* remove(node* p, int k) 
+{
+	if (!p) return 0;
+	if (k < p->key)
+		p->left = remove(p->left, k);
+	else if (k > p->key)
+		p->right = remove(p->right, k);
+	else //  k == p->key 
+	{
+		node* q = p->left;
+		node* r = p->right;
+		delete p;
+		if (!r) return q;
+		node* min = findmin(r);
+		min->right = removemin(r);
+		min->left = q;
+		return balance(min);
+	}
+	return balance(p);
+}
+int main()
+{
+	std::cout << "Hello World!\n";
+}
