b63cdd66e442de0830f44e10f7aea65d14da1fa5
olegzig <boris37544@gmail.com>
2024-05-05 17:45:40 +0300

    Снёс всё ненужное

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -74,60 +74,20 @@ class AVLTree {
 	AVLTreeNode<T>* root;
 
 	void balance(vector<AVLTreeNode<T>**> path);
-	void display(AVLTreeNode<T>* cur, int depth = 0, int state = 0);
 
 public:
 	AVLTree();
-	~AVLTree();
 
 	void insert(T value);
-	void erase(T value);
-
-	void clear();
-	bool empty() const;
-	int size() const;
 
 	int find(T value) const;
-	int lower_bound(T value) const;
-	int upper_bound(T value) const;
 	const T& operator[](size_t idx) const;
-
-	void display();
 };
 
 template <class T>
 AVLTree<T>::AVLTree() {
 	root = nullptr;
 	_size = 0;
-}
-
-template <class T>
-AVLTree<T>::~AVLTree() {
-	clear();
-}
-
-template <class T>
-void AVLTree<T>::clear() {
-	vector<AVLTreeNode<T>*> stack;
-
-	if (root != nullptr)
-		stack.push_back(root);
-
-	while (!stack.empty()) {
-		AVLTreeNode<T>* node = stack.back();
-		stack.pop_back();
-
-		if (node->left != nullptr)
-			stack.push_back(node->left);
-
-		if (node->right != nullptr)
-			stack.push_back(node->right);
-
-		_size--;
-		delete node;
-	}
-
-	root = nullptr;
 }
 
 template <class T>
@@ -149,76 +109,6 @@ void AVLTree<T>::insert(T value) {
 
 	balance(path);
 	_size++;
-}
-
-template <class T>
-void AVLTree<T>::erase(T value) {
-	AVLTreeNode<T>** indirect = &root;  // to generalize insertion
-	vector<AVLTreeNode<T>**> path;  // to update height values
-
-	while (*indirect != nullptr and (*indirect)->value != value) {
-		path.push_back(indirect);
-
-		if ((*indirect)->value > value)
-			indirect = &((*indirect)->left);
-		else
-			indirect = &((*indirect)->right);
-	}
-
-	if (*indirect == nullptr)  // the value does not exist in tree
-		return;  // may be raising an Exception is more elegant
-
-	else
-		path.push_back(indirect);
-
-	size_t index = path.size();
-
-	if ((*indirect)->left == nullptr and (*indirect)->right == nullptr) {  // the node is leaf
-		delete* indirect;  // just delete node
-		*indirect = nullptr;
-		path.pop_back();  // pop the deleted node from path
-	}
-
-	else if ((*indirect)->right == nullptr) {  // only left child exists
-		AVLTreeNode<T>* toRemove = *indirect;
-
-		(*indirect) = (*indirect)->left;
-		delete toRemove;
-
-		path.pop_back();
-	}
-
-	else {  // right child exists
-		AVLTreeNode<T>** successor = &((*indirect)->right);
-
-		while ((*successor)->left != nullptr) {
-			path.push_back(successor);
-			successor = &((*successor)->left);
-		}
-
-		if (*successor == (*indirect)->right) {
-			(*successor)->left = (*indirect)->left;
-
-			AVLTreeNode<T>* toRemove = *indirect;
-			*indirect = *successor;
-			delete toRemove;
-		}
-
-		else {
-			AVLTreeNode<T>* tmp = *path.back(), * suc = *successor;
-
-			tmp->left = (*successor)->right;
-			suc->left = (*indirect)->left;
-			suc->right = (*indirect)->right;
-
-			delete* indirect;
-			*indirect = suc;
-			path[index] = &(suc->right);
-		}
-	}
-
-	balance(path);
-	_size--;
 }
 
 template <class T>
@@ -247,16 +137,6 @@ void AVLTree<T>::balance(vector<AVLTreeNode<T>**> path) {  // starting from root
 }
 
 template <class T>
-bool AVLTree<T>::empty() const {
-	return _size == 0;
-}
-
-template <class T>
-int AVLTree<T>::size() const {
-	return _size;
-}
-
-template <class T>
 int AVLTree<T>::find(T value) const {
 	AVLTreeNode<T>* direct = root;  // to generalize insertion
 	int idx = 0;
@@ -275,40 +155,6 @@ int AVLTree<T>::find(T value) const {
 
 	else
 		return idx + (direct->left ? direct->left->count : 0);
-}
-
-template <class T>
-int AVLTree<T>::upper_bound(T value) const {
-	AVLTreeNode<T>* direct = root;  // to generalize insertion
-	int idx = 0;
-
-	while (direct != nullptr) {
-		if (direct->value > value)
-			direct = direct->left;
-		else {
-			idx += (direct->left ? direct->left->count : 0) + 1;
-			direct = direct->right;
-		}
-	}
-
-	return idx;
-}
-
-template <class T>
-int AVLTree<T>::lower_bound(T value) const {
-	AVLTreeNode<T>* direct = root;  // to generalize insertion
-	int idx = 0;
-
-	while (direct != nullptr) {
-		if (direct->value >= value)
-			direct = direct->left;
-		else {
-			idx += (direct->left ? direct->left->count : 0) + 1;
-			direct = direct->right;
-		}
-	}
-
-	return idx;
 }
 
 template <class T>
@@ -331,35 +177,6 @@ const T& AVLTree<T>::operator[](size_t idx) const {
 	}
 
 	return cur->value;
-}
-
-template <class T>
-void AVLTree<T>::display() {
-	printf("\n");
-	if (root != nullptr)
-		display(root);
-	else
-		printf("Empty");
-	printf("\n");
-}
-
-template <class T>
-void AVLTree<T>::display(AVLTreeNode<T>* cur, int depth, int state) {  // state: 1 -> left, 2 -> right , 0 -> root
-	if (cur->left)
-		display(cur->left, depth + 1, 1);
-
-	for (int i = 0; i < depth; i++)
-		printf("     ");
-
-	if (state == 1) // left
-		printf("┌───");
-	else if (state == 2)  // right
-		printf("└───");
-
-	cout << "[" << cur->value << "] - (" << cur->count << ", " << cur->height << ")" << endl;
-
-	if (cur->right)
-		display(cur->right, depth + 1, 2);
 }
 
 int main()
@@ -423,6 +240,5 @@ int main()
 	cin >> input;
 	int result = tree.find(stoi(input));
 	cout << "index of element is: " << result;
-	tree.clear();
 	return 0;
 }
